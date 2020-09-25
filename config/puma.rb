66bfd7071f36eeace557c4507ef0913c965dbb78
rambor <robert_p_rambo@diamond.ac.uk>
2020-09-25 09:03:41 +0100

    Heroku log says using RAILS_ENV not RACK_ENV (website posts RACK_ENV)

--- config/puma.rb
+++ config/puma.rb
@@ -16,8 +16,8 @@ port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-# environment ENV.fetch("RAILS_ENV") { "development" }
-environment ENV['RACK_ENV'] || 'development'
+environment ENV.fetch("RAILS_ENV") { "development" }
+#environment ENV['RACK_ENV'] || 'development'
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
